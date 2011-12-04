63053385a233121e84068f2118e0daaf77d0298e
Attila Lendvai <attila@lendvai.name>
2011-12-04 13:47:09 +0000

    make application-with-perec-support bind the database in a broader scope

--- integration/perec.lisp
+++ integration/perec.lisp
@@ -9,11 +9,14 @@
 (def (class* e) application-with-perec-support (application)
   ((database nil)))
 
+(def method produce-response :around ((application application-with-perec-support) request)
+  (hu.dwim.rdbms:with-database (database-of application)
+    (call-next-method)))
+
 (def method call-in-application-environment :around ((application application-with-perec-support) session thunk)
   (bind ((database (database-of application)))
     (unless database
       (error "No database is specified for ~A" application))
-    (hu.dwim.rdbms:with-database database
-      (hu.dwim.rdbms:with-readonly-transaction
-        (hu.dwim.perec:with-new-compiled-query-cache
-          (call-next-method))))))
+    (hu.dwim.rdbms:with-readonly-transaction
+      (hu.dwim.perec:with-new-compiled-query-cache
+        (call-next-method)))))
